d10449e9976cd2bf4d98951e7487605601a8cf8d
Antonio Bejarano <pandaman1441@outlook.com>
2024-08-06 03:48:10 -0600

    i believe the convert instructions are working

--- src/main/kotlin/templates/Convert_Byte_to_ASCII.kt
+++ src/main/kotlin/templates/Convert_Byte_to_ASCII.kt
@@ -3,10 +3,23 @@ import templates.Instruction
 
 class Convert_Byte_to_ASCII : Instruction() {
     override fun organizeBytes(instruction: IntArray): IntArray {
-        TODO("Not yet implemented")
+        return instruction
     }
 
     override fun operation(bytes: IntArray): Int {
-        TODO("Not yet implemented")
+        val rX = bytes[1]
+        val rXValue = Emulator.cpu.registers[rX]
+
+        if (rXValue > 0xF){
+            throw IllegalArgumentException("Value in r$rX is greater than 0xF")
+        }
+        val asciiValue = if (rXValue in 0..9){
+            rXValue + 0x30
+        } else {
+            rXValue + 0x37
+        }
+
+        Emulator.cpu.registers[bytes[2]] = rXValue
+        return 2
     }
 }
--- src/main/kotlin/templates/Convert_to_Base_10.kt
+++ src/main/kotlin/templates/Convert_to_Base_10.kt
@@ -2,10 +2,25 @@ import templates.Instruction
 
 class Convert_to_Base_10 : Instruction() {
     override fun organizeBytes(instruction: IntArray): IntArray {
-        TODO("Not yet implemented")
+        return instruction
     }
 
     override fun operation(bytes: IntArray): Int {
-        TODO("Not yet implemented")
+        val rXValue = Emulator.cpu.registers[bytes[1]]
+        val hundreds = rXValue / 100
+        val tens = (rXValue / 10) % 10
+        val ones = rXValue % 10
+
+        if (Emulator.cpu.memory == 0){
+            Emulator.ram.write(Emulator.cpu.address, hundreds.toUByte())
+            Emulator.ram.write(Emulator.cpu.address+1, tens.toUByte())
+            Emulator.ram.write(Emulator.cpu.address+2, ones.toUByte())
+        }
+        else {
+            Emulator.rom.write(Emulator.cpu.address,hundreds.toUByte())
+            Emulator.rom.write(Emulator.cpu.address+1,tens.toUByte())
+            Emulator.rom.write(Emulator.cpu.address+2,ones.toUByte())
+        }
+        return 2
     }
 }
